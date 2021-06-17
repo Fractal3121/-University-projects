d2cbeab00204dde74dae8e1c21fdd069fa48700c
Fractal3121 <76220559+Fractal3121@users.noreply.github.com>
2021-06-17 18:53:22 +0300

    My university projects

---
+++ IT/lab/src/main/java/webdbms/controller/TableController.java
@@ -0,0 +1,116 @@
+package webdbms.controller;
+
+import org.springframework.web.bind.annotation.*;
+import webdbms.DBMS.datatype.DataType;
+import webdbms.DBMS.datatype.constraint.RealConstraint;
+import webdbms.DBMS.exception.StorageException;
+import webdbms.DBMS.table.Table;
+import webdbms.DBMS.table.TableFactory;
+import webdbms.facades.DatabaseFacade;
+import webdbms.facades.EntryFacade;
+import webdbms.facades.TableFacade;
+import webdbms.service.DatabaseService;
+import webdbms.service.TableService;
+import webdbms.service.exception.InternalServerException;
+import webdbms.service.exception.InvalidRequestBodyException;
+import java.util.*;
+
+@RestController
+@RequestMapping("/databases/{databaseName}/tables")
+public class TableController {
+
+    private DatabaseService databaseService;
+    private TableService tableService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Collection<String> getAllTables(@PathVariable String databaseName) {
+        try {
+            return new DatabaseFacade(databaseService.findByName(databaseName))
+                    .getTableNames();
+        } catch (StorageException e) {
+            throw new InternalServerException(e);
+        }
+    }
+
+    @RequestMapping(value = "/{tableName}", method = RequestMethod.GET)
+    public TableFacade getTable(@PathVariable String databaseName,
+                                @PathVariable String tableName) {
+        try {
+            Table table = tableService.findByName(databaseName, tableName);
+            return new TableFacade(table);
+        } catch (StorageException e) {
+            throw new InternalServerException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void createTable(@PathVariable String databaseName,
+                            @RequestBody Map<String, Object> requestBody) {
+        try {
+            String name = requestBody.get("tableName").toString();
+            List<DataType> types = getTypesFromObjects((List<Object>) requestBody.get("columnTypes"));
+            RealConstraint constraint = getConstraintFromMap((Map) requestBody.get("realIntervalConstraint"));
+            List<String> columnNames = getColumnNamesFromObjects((List<Object>) requestBody.get("columnNames"));
+            tableService.createTable(databaseName, name, types, columnNames, constraint);
+        } catch (ClassCastException | NullPointerException e) {
+            throw new InvalidRequestBodyException();
+        } catch (StorageException e) {
+            throw new InternalServerException(e);
+        }
+    }
+
+
+    @RequestMapping(value = "/{tableName}", method = RequestMethod.PUT)
+    public void renameColumn(@PathVariable String databaseName,
+                             @RequestBody Map<String, Object> requestBody, @PathVariable String tableName) {
+        try {
+            List<String> columnNames = getColumnNamesFromObjects((List<Object>) requestBody.get("columnNames"));
+            //tableService.createTable(databaseName, name, types, columnNames, constraint);
+            tableService.findByName(databaseName, tableName).changeColumnName(columnNames);
+        } catch (ClassCastException | NullPointerException e) {
+            throw new InvalidRequestBodyException();
+        } catch (StorageException e) {
+            throw new InternalServerException(e);
+        }
+    }
+
+    private List<DataType> getTypesFromObjects(List<Object> objects) {
+        List<DataType> types = new ArrayList<>();
+        objects.forEach(type -> types.add(DataType.valueOf(type.toString())));
+        return types;
+    }
+
+    private RealConstraint getConstraintFromMap(Map<String, Object> map) {
+        if (map.get("maxValue") == null && map.get("minValue") == null) {
+            return new RealConstraint();
+        }
+        double maxValue = map.get("maxValue") instanceof Integer ? (int)map.get("maxValue") : (double)map.get("maxValue");
+        double minValue = map.get("minValue") instanceof Integer ? (int)map.get("minValue") : (double)map.get("minValue");
+        return new RealConstraint(minValue, maxValue);
+    }
+
+    private List<String> getColumnNamesFromObjects(List<Object> objects) {
+        List<String> names = new ArrayList<>();
+        objects.forEach(colName -> names.add(colName.toString()));
+        return names;
+    }
+
+    @RequestMapping(value = "/{tableName}", method = RequestMethod.DELETE)
+    public void deleteTable(@PathVariable String databaseName,
+                            @PathVariable String tableName) {
+        try {
+            tableService.deleteTable(databaseName, tableName);
+        } catch (StorageException e) {
+            throw new InternalServerException(e);
+        }
+    }
+
+    public void setDatabaseService(DatabaseService databaseService) {
+        this.databaseService = databaseService;
+    }
+
+    public void setTableService(TableService tableService) {
+        this.tableService = tableService;
+    }
+}
